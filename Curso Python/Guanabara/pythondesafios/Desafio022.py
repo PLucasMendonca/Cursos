c157603d9ce80ad6d96e5640727cf4f53a58a58c
Lucas Mendonça <p.lucas.santos.mendonca@gmail.com>
2022-01-26 17:57:26 -0300

    Novas atividades

---
+++ Curso Python/Guanabara/pythondesafios/Desafio022.py
@@ -0,0 +1,5 @@
+'''Crie um programa completo de uma pessoa e mostre:
+*O nome com todas as letras maiúscula.
+*O nome com todas minúsculas.
+*Quantas letras ao todo (sem considerar espaços).
+*Quantas letras tem o primeiro nome.'''
