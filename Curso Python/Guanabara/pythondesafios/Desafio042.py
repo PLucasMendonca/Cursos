4368db3834f793489f7fde470971b5c061b5a299
Lucas Mendonça <p.lucas.santos.mendonca@gmail.com>
2022-03-21 20:13:11 -0300

    Novas atividades

---
+++ Curso Python/Guanabara/pythondesafios/Desafio042.py
@@ -0,0 +1,4 @@
+'''Refaça o DESAFIO 035 dos triãngulos, acrescentando o recurso de mostrar que tipo de triãngulo será formado:
+-Equilátero: todaos os lados iguais
+-Isóceles:dois lados iguais
+-Escaleno: todos os lados diferentes'''
