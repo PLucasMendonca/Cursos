cd2898eef4994b1b37ff0e424417f1948999c20d
Lucas Mendonça <p.lucas.santos.mendonca@gmail.com>
2025-09-07 19:14:53 -0300

    Ajustando atividades de for e while

---
+++ Curso Python/Alura/Formacao/Praticando_Python/lacos_for_e_while/calculando_a_soma_de_numeros.py
@@ -0,0 +1,5 @@
+'''Você está recebendo uma lista de valores representando os produtos de sua loja virtual e gostaria de calcular a soma total desses produtos para entender o desempenho financeiro semanal.
+
+valores = [10, 20, 30, 40, 50]
+
+Crie um programa para implementar a soma.'''
