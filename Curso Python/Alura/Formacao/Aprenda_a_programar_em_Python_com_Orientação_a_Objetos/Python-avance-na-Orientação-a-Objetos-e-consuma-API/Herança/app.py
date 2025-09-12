3bd474c61275603cf6b3e87fba8942b217d8c36a
Lucas Mendonça <p.lucas.santos.mendonca@gmail.com>
2025-09-11 21:11:53 -0300

    Novas atividades

---
+++ Curso Python/Alura/Formacao/Aprenda_a_programar_em_Python_com_Orientação_a_Objetos/Python-avance-na-Orientação-a-Objetos-e-consuma-API/Herança/app.py
@@ -0,0 +1,13 @@
+from modulo.restaurante import Restaurante
+from modulo.cardapio.bebida import Bebida
+from modulo.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'Gourmert')
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+prato_pao = Prato('Pãozinho', 2, 'O melhor pão da cidade')
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
